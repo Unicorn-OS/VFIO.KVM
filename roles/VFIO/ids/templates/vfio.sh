d137233f7c406698c497205861bf7b6512a8ddee
Dark Star <darkstar@unisocial.net>
2020-12-24 19:59:43 -0600

    Refactor using OS/distro/version paths

--- roles/VFIO/ids/templates/vfio.sh
+++ roles/VFIO/ids/templates/vfio.sh
@@ -16,7 +16,7 @@ esac
 
 # Need to make this use ansible Lists instead!
 #
-for dev in {{device0}} {{device1}}
+for dev in {{device0}} {{device1}} {{device2}}
 do
  echo "vfio-pci" > /sys/bus/pci/devices/$dev/driver_override
  echo "$dev" > /sys/bus/pci/drivers/vfio-pci/bind
